8927d83ef51921eea0aa49797eff3975e1949055
Brad Hilton <brad.hilton.nw@gmail.com>
2016-05-17 19:25:14 -0600

    Initial Commit

---
+++ August/August.h
@@ -0,0 +1,19 @@
+//
+//  August.h
+//  August
+//
+//  Created by Bradley Hilton on 5/17/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for August.
+FOUNDATION_EXPORT double AugustVersionNumber;
+
+//! Project version string for August.
+FOUNDATION_EXPORT const unsigned char AugustVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <August/PublicHeader.h>
+
+
